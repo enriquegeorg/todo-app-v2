4bf7b7ce4cd191b618cc615528561980a70e8871
Enrique Georg <enriquegeorg@gmail.com>
2022-05-02 22:31:08 -0300

    fix webpack config to use last vestion deps

---
+++ frontend/src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOMClient from "react-dom/client";
+import App from "./main/app";
+
+const container = document.getElementById('app');
+const root = ReactDOMClient.createRoot(container);
+root.render(<App tab="home" />);
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,41 +1,45 @@
-const webpack = require('webpack');
-const ExtractTextPlugin = require('mini-css-extract-plugin');
+const webpack = require("webpack");
+const MiniCSSExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        path: __dirname + '/public',
-        filename: './app.js'
+  entry: "./src/index.jsx",
+  output: {
+    path: __dirname + "/public",
+    filename: "./app.js",
+  },
+  mode: "development",
+  devServer: {
+    static: {
+      directory: "./public",
     },
-    devServer: {
-        port: 8080,
-        contentBase: './public'
+    port: 8080,
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    alias: {
+      "@": __dirname + "/src",
+      modules: __dirname + "/node_modules",
     },
-    resolve: {
-        extensions: ['' , '.js', '.jsx'],
-        alias: {
-            '@': __dirname + '/src',
-            modules: __dirname + '/node_modules'
-        }
-    },
-    plugins: [
-        new ExtractTextPlugin('app.css'),
+  },
+  plugins: [new MiniCSSExtractPlugin({ filename: "app.css" })],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          plugins: ["transform-object-rest-spread"],
+        },
+      },
+      {
+        test: /\.woof|\.woff|\.woff2|\.ttf|\.eot|\.svg$/,
+        loader: "file-loader",
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCSSExtractPlugin.loader, "css-loader"],
+      },
     ],
-    module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['react', 'es2015'],
-                plugins: ['transform-object-rest-spread']
-            }
-        }, {
-            test: /\.css$/,
-            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-        }, {
-            test: /\.woof|\.woff|\.woff2|\.ttf|\.eot|\.svg$/,
-            loader: 'file'
-        }]
-    }
-}
+  },
+};
